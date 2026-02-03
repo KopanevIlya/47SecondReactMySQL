4b937fa34c38af4bda48d7b8f430e69e7647642a
KopanevIlya <ilya.kopanev@mail.ru>
2026-02-03 20:22:13 +0300

    create header

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
+
 import Advantage from "./components/Advantage/Advantage";
 import Group from "./components/Group 213/Group";
-
-
-
-
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+    <Header/>
     <Group/>
     <Advantage/>
+
     </div>
   );
 }
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaBars, FaPhone, FaShoppingCart, FaLocationArrow } from "react-icons/fa";
+import "./Header.css";
+
+const Header = () => (
+  <header className="header">
+    <div className="header__top">
+      <div className="header__logo-block">
+        <img src="/Group.png" alt="ОфисДирект" className="header__logo" />
+      </div>
+      <input
+        type="text"
+        placeholder="Название модели или артикул"
+        className="header__search"
+      />
+      <div className="header__contacts">
+        <div className="header__phone">
+          <FaPhone className="header__phone-icon" />
+          <span>+7 495 221-06-75</span>
+        </div>
+        <button className="header__callback">Заказать звонок</button>
+      </div>
+    </div>
+    <div className="header__bottom">
+      <div className="header__menu">
+        <button className="header__catalog">
+          <FaBars /> Каталог
+        </button>
+        <a href="#" className="header__link">О продукте</a>
+        <a href="#" className="header__link">Отзывы</a>
+        <a href="#" className="header__link">Доставка и оплата</a>
+        <a href="#" className="header__link">Контакты</a>
+      </div>
+      <div className="header__actions">
+        <div className="header__city">
+          <FaLocationArrow />
+          <span>Москва</span>
+        </div>
+        <div className="header__cart">
+          <FaShoppingCart />
+          <span>Моя корзина</span>
+          <span className="header__cart-count">+1</span>
+        </div>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
+
